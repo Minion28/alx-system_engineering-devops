d12229f07410d59c3f6f793be9510422f462e344
Kelvin Mwanzia <wambuakelvin14@gmail.com>
2022-12-05 01:35:20 +0300

    strace is your friend

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,7 @@
 # fix apache returning 500 error
-exec{'fix-wordpress':
+
+
+exec{ 'fix-wordpress':
     command => 'sed -i "s/phpp/php" /var/www/html/wp-settings.php',
     path    => '/usr/local/bin/:/bin/'
     }
